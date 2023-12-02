629d8fbd83fb101ef6c01600314a11405eb9f50b
Teschl <69400012+Teschl@users.noreply.github.com>
2023-12-02 10:39:36 +0100

    day01

---
+++ day01/calibration.py
@@ -0,0 +1,47 @@
+import re
+
+def aufg1():
+
+    filePath = "day01/input.txt"
+    file = open(filePath)
+    lines = file.readlines()
+    file.close()
+
+    sum = 0
+    for i in lines:
+        i = i.strip()
+        i = re.sub("[a-z]", "", i)
+        result = i[0] + i[len(i)-1]
+        sum = sum + int(result)
+
+    return sum
+
+def aufg2():
+    filePath = "day01/input.txt"
+    file = open(filePath)
+    lines = file.readlines()
+    file.close()
+    
+    sum = 0
+    for i in lines:
+        i = i.strip()
+
+        i = re.sub("zero","0o",i)
+        i = re.sub("one","o1e",i)
+        i = re.sub("two","t2o",i)
+        i = re.sub("three","t3e",i)
+        i = re.sub("four","f4r",i)
+        i = re.sub("five","f5e",i)
+        i = re.sub("six","s6x",i)
+        i = re.sub("seven","s7n",i)
+        i = re.sub("eight","e8t",i)
+        i = re.sub("nine","n9e",i)
+
+        i = re.sub("[a-z]", "", i)
+        result = i[0] + i[len(i)-1]
+        sum = sum + int(result)
+
+    return sum
+
+print(aufg1())
+print(aufg2())
