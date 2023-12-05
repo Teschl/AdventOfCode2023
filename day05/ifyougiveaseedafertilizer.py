767ed59f07236a7f9572288cefb5477801f518d3
Teschl <69400012+Teschl@users.noreply.github.com>
2023-12-05 08:30:19 +0100

    day05 init

---
+++ day05/ifyougiveaseedafertilizer.py
@@ -0,0 +1,20 @@
+
+def aufg1():
+
+    filePath = "day01/input.txt"
+    file = open(filePath)
+    lines = file.readlines()
+    file.close()
+
+
+def aufg2():
+
+    filePath = "day01/input.txt"
+    file = open(filePath)
+    lines = file.readlines()
+    file.close()
+
+
+
+print(aufg1())
+print(aufg2())
