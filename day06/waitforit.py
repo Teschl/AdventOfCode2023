e244a9dfbea951ab492ce80f5d3806f880f9bb66
Teschl <69400012+Teschl@users.noreply.github.com>
2023-12-06 11:32:16 +0100

    day06 done

--- day06/waitforit.py
+++ day06/waitforit.py
@@ -1,18 +1,50 @@
 import re
 
 def aufg1():
-    filePath = "day05/input.txt"
+    filePath = "day06/input.txt"
     file = open(filePath)
     lines = file.readlines()
     file.close()
 
+    time = re.split(" +", re.sub("Time: *","",lines[0].strip()))
+    distance = re.split(" +", re.sub("Distance: *","",lines[1].strip()))
+
+    time = list(map(int, time))
+    distance = list(map(int, distance))
+
+    # time      = hold + move
+    # distance  < hold * move
+    # distance  < hold * (t - hold)
+
+    result = 1
+    for i in range(0,len(time)):
+        wins = 0
+        for hold in range(0,time[i]):
+            if distance[i] < hold * (time[i] - hold):
+                wins += 1
+        result = result*wins
+
+    return result
 
 def aufg2():
-    filePath = "day05/input.txt"
+    filePath = "day06/input.txt"
     file = open(filePath)
     lines = file.readlines()
     file.close()
+    
+    time = int(re.sub("Time:| *","",lines[0].strip()))
+    distance = int(re.sub("Distance:| *","",lines[1].strip()))
 
+    # time      = hold + move
+    # distance  < hold * move
+    # distance  < hold * (t - hold)
+
+    wins = 0
+    for hold in range(0,time):
+        if distance < hold * (time - hold):
+            wins += 1
+
+    return wins
 
 
 print(aufg1())
