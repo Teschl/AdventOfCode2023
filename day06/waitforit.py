53790b634717a290a1e026e6ae11e11461d9466c
Teschl <69400012+Teschl@users.noreply.github.com>
2023-12-06 10:21:35 +0100

    day06 init

--- day06/waitforit.py
+++ day06/waitforit.py
@@ -6,12 +6,6 @@ def aufg1():
     lines = file.readlines()
     file.close()
 
-    # get seeds from input
-    seeds = re.split(" ", re.sub("seeds: ","", lines[0].strip()))
-
-    
-        
-
 
 def aufg2():
     filePath = "day05/input.txt"
