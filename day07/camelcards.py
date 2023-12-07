69e51c3e1842f429241308eba087bae6ba114d7a
Teschl <69400012+Teschl@users.noreply.github.com>
2023-12-07 12:25:34 +0100

    day07 init

---
+++ day07/camelcards.py
@@ -0,0 +1,19 @@
+import re
+
+def aufg1():
+    filePath = "day07/input.txt"
+    file = open(filePath)
+    lines = file.readlines()
+    file.close()
+
+import re
+
+def aufg2():
+    filePath = "day07/input.txt"
+    file = open(filePath)
+    lines = file.readlines()
+    file.close()
+
+
+print(aufg1())
+print(aufg2())
